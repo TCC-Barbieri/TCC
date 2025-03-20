e73e99c211ab7f358bc0cb2d4e0cc7a83c3d767f
tiago sbardelini <tiagosbardelinifgc@gmail.com>
2025-03-20 15:59:25 -0300

    LoginPage

--- TCC/App.xaml.cs
+++ TCC/App.xaml.cs
@@ -1,15 +1,24 @@
-﻿namespace TCC
+﻿using TCC.Views;
+
+namespace TCC
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            MainPage = new LoginPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            Window window = base.CreateWindow(activationState);
+
+            window.Width = 800;
+            window.Height = 600;
+
+            return window;
         }
     }
 }
